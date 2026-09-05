ddf16459d7cc2b921776e1381835a61cd11b62e5
Vikas Yadav <vikas.yadav.322005@gmail.com>
2026-09-05 19:07:37 +0530

    Fix Flask project structure

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 
 # load databasedataset===================================
-sym_des = pd.read_csv("datasets/symtoms_df.csv")
+sym_des = pd.read_csv("datasets/symptoms_df.csv")
 precautions = pd.read_csv("datasets/precautions_df.csv")
 workout = pd.read_csv("datasets/workout_df.csv")
 description = pd.read_csv("datasets/description.csv")
